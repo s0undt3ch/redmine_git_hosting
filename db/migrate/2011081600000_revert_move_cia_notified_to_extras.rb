365d9a675d45d47ea43a2cc656dc9e55aeb5bc6f
Pedro Algarvio <pedro@algarvio.me>
2011-08-16 19:36:40 +0100

    Revert move cia_notified to extras table

---
+++ db/migrate/2011081600000_revert_move_cia_notified_to_extras.rb
@@ -0,0 +1,30 @@
+class RevertMoveCiaNotifiedToExtras < ActiveRecord::Migration
+	def self.up
+
+		add_column :changesets, :notified_cia, :integer, :default=>0
+
+		Project.find(:all).each {|project|
+			if project.repository.is_a?(Repository::Git)
+				project.repository.changesets.each { |revision|
+					if project.repository.extra.notified_cia.include? revision.scmid
+						revision.notified_cia = 1
+					else
+						revision.notified_cia = 0
+					end
+					revision.save
+				}
+			end
+		}
+
+		remove_column :git_repository_extras, :notified_cia
+	end
+
+	def self.down
+		remove_column :changesets, :notified_cia
+		add_column :git_repository_extras, :notified_cia, :text, :default=>[]
+	end
+
+	def self.table_exists?(name)
+		ActiveRecord::Base.connection.tables.include?(name)
+	end
+end
